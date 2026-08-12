5b7bb221bcf31c743adb0e7c6e52eded86558b97
Macha Sneha <snehamacha30@gmail.com>
2026-08-12 18:28:24 +0530

    Add automatic footer year update

--- WebDev-L2-PersonalPortfolio/script.js
+++ WebDev-L2-PersonalPortfolio/script.js
@@ -1,1 +1,7 @@
+// Update the footer year automatically
+const footerText = document.querySelector("footer p");
 
+if (footerText) {
+    const currentYear = new Date().getFullYear();
+    footerText.textContent = `© ${currentYear} Macha Sneha. All Rights Reserved.`;
+}
