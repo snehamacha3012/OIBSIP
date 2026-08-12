179e5862994487e2dcf4c302b8023c659b8480e0
Macha Sneha <snehamacha30@gmail.com>
2026-08-12 22:02:07 +0530

    Delete WebDev-L2-PersonalPortfolio/script.js

--- WebDev-L2-PersonalPortfolio/script.js
+++
@@ -1,7 +0,0 @@
-// Update the footer year automatically
-const footerText = document.querySelector("footer p");
-
-if (footerText) {
-    const currentYear = new Date().getFullYear();
-    footerText.textContent = `© ${currentYear} Macha Sneha. All Rights Reserved.`;
-}
